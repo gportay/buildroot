dc8df12c854c550a57294ff8f99b0af765fa7743
Gaël PORTAY <gael.portay@savoirfairelinux.com>
2018-04-08 09:13:09 -0400

    package: add host-debootstrap
    Signed-off-by: Gaël PORTAY <gael.portay@savoirfairelinux.com>

---
+++ package/debootstrap/debootstrap.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# debootstrap
+#
+################################################################################
+
+DEBOOTSTRAP_VERSION = 9c57a72af07f32eec59e56b86f6375bca9228ddc
+DEBOOTSTRAP_SITE = git://anonscm.debian.org/d-i/debootstrap.git
+DEBOOTSTRAP_LICENSE = MIT
+DEBOOTSTRAP_LICENSE_FILES = debian/copyright
+
+define HOST_DEBOOTSTRAP_INSTALL_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE1) DESTDIR="$(TARGET_DIR)" -C $(@D) install
+endef
+
+define HOST_DEBOOTSTRAP_INSTALL_CMDS
+	$(INSTALL) -d -m 0755 $(HOST_DIR)/bin/ $(HOST_DIR)/share/debootstrap/scripts
+	cp -a $(@D)/scripts/* $(HOST_DIR)/share/debootstrap/scripts/
+	$(INSTALL) -m 0644 $(@D)/functions $(HOST_DIR)/share/debootstrap/
+	VERSION=$$(sed 's/.*(\(.*\)).*/\1/; q' $(@D)/debian/changelog); \
+	sed "s/@VERSION@/$$VERSION/g" $(@D)/debootstrap >$(HOST_DIR)/bin/debootstrap
+	chmod 0755 $(HOST_DIR)/bin/debootstrap
+	sed -e "s,/usr/share/debootstrap,$(HOST_DIR)/share/debootstrap,g" \
+	    -i $(HOST_DIR)/share/debootstrap/scripts/* $(HOST_DIR)/bin/debootstrap
+endef
+
+$(eval $(host-generic-package))
